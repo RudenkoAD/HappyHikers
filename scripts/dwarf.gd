5bd7602802d6abb2abbb4fced197343a37eb1829
RudenkoAD <rudenko.ad@phystech.edu>
2024-07-26 18:17:00 +0300

    dwarf

--- scripts/dwarf.gd
+++ scripts/dwarf.gd
@@ -6,6 +6,7 @@ var starting_jump := false
 var timer
 
 @onready var ability_detector = $AbilityDetector
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -200.0 * 1.5
@@ -15,7 +16,7 @@ const CHARACTER_GRAVITY_MULTIPLIER = 1.2
 
 var mult_placeholder = 0
 var is_ability_active = false
-
+var looking_right = true
 
 func _ready():
 	timer = Timer.new()
@@ -23,11 +24,17 @@ func _ready():
 	timer.one_shot = true
 	timer.set_wait_time(1)
 	timer.connect("timeout", self._on_timer_timeout)
+	animated_sprite_2d.set_animation("idle")
+	animated_sprite_2d.play()
+
+
 
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("dwarf_ability") and ability_detector.has_overlapping_bodies():
 		is_ability_active = !is_ability_active
+		animated_sprite_2d.set_animation("grip")
+		animated_sprite_2d.play()
 		velocity.y = 0
 	if !is_ability_active:
 		if not is_on_floor():
@@ -50,7 +57,15 @@ func _physics_process(delta):
 			velocity.x = direction * SPEED
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
-			
+		
+		if (velocity.x<0 and looking_right) or (velocity.x>0 and !looking_right):
+				self.scale.x = -abs(self.scale.x)
+				looking_right = !looking_right
+		
+		if velocity == Vector2.ZERO:
+			animated_sprite_2d.set_animation("idle")
+		else:
+			animated_sprite_2d.set_animation("walk")
 		move_and_slide()
 
 func _on_timer_timeout():
